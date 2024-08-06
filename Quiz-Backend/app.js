ec13d7ba9d72bee2a929986493695c141d60b701
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-08-06 22:05:47 +0530

    Update app.js

--- Quiz-Backend/app.js
+++ Quiz-Backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://quiz-app-pro.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
