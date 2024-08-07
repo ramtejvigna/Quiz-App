d8e6db8c641b9f5faedc61873d8042ff33f9cdbb
ramtejvigna <vignaramtejtelagarapu@gmail.com>
2024-08-07 22:32:21 +0530

    Updated SignUp

--- Quiz-Frontend/src/components/SignUp.jsx
+++ Quiz-Frontend/src/components/SignUp.jsx
@@ -161,7 +161,7 @@ export default function SignUp() {
               </Button>
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link to="/signin" variant="body2">
+                  <Link to="/signin" variant="body2" >
                     Already have an account? Sign in
                   </Link>
                 </Grid>
